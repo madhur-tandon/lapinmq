fe3770ff9322c33840b8c1a7f3c3253e599c9a7a
Madhur Tandon <madhurtandon23@gmail.com>
2023-09-30 01:36:13 +0530

    release to PyPI action

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
+import os
 from setuptools import setup
+
+def read_version():
+    with open("lapinmq/version", "r") as f:
+        return f.read().strip("\n")
+
+def check_tag_version():
+    tag = os.getenv("GITHUB_REF")
+    expected_version = read_version()
+    if tag != f"refs/tags/v{expected_version}":
+        raise Exception(f"Tag '{tag}' does not match the expected "
+                        f"version '{expected_version}'")
+
+check_tag_version()
 
 setup(
     name='lapinmq',
-    version='0.1.0',    
+    version=read_version(),
     description='Utilities for RabbitMQ following best practices',
     url='https://github.com/madhur-tandon/lapinmq',
     author='Madhur Tandon',
