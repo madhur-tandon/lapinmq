842d214d6bbe5967f84537f2a34a49e491a021e7
Madhur Tandon <20173739+madhur-tandon@users.noreply.github.com>
2023-09-29 11:55:58 +0530

    add wait for sigterm

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import pika
+import signal
+import threading
 
 def alert_and_crash(message):
     print(message)
@@ -16,3 +18,25 @@ def get_parameters(blocked_connection_timeout=3600):
         credentials=pika.PlainCredentials(os.getenv("RABBITMQ_USERNAME"), os.getenv("RABBITMQ_PASSWORD")),
         blocked_connection_timeout=blocked_connection_timeout,
     )
+
+class SigtermHandler:
+    def __init__(self):
+        self.sigterm_received = False
+        self.lock = threading.Lock()
+        self.condvar = threading.Condition(self.lock)
+        signal.signal(signal.SIGTERM, self.signal_handler)
+
+    def signal_handler(self, signum, stack_frame):
+        with self.lock:
+            self.sigterm_received = True
+            self.condvar.notify()
+
+    def wait_for_sigterm(self, timeout):
+        with self.lock:
+            self.condvar.wait(timeout_secs)
+        return self.sigterm_received
+
+sigterm_handler = SigtermHandler()
+
+def wait_for_sigterm(timeout = None):
+    return sigterm_handler.wait_for_sigterm(timeout)
